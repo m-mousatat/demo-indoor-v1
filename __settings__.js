0b6937153404c6ee36d822d9291d284862441311
m-mousatat <m.mousatat@innopolis.university>
2024-03-06 02:23:23 +0300

    Full demo version 1

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1950246.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 167276885, 167282742 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/167276987/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/167276988/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/167276986/1/ammo.js', 'preload' : true},
+];
